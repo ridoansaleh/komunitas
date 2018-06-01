daada3d87aeca380dc148a1568d66dee3b81ff35
ridoansaleh <ridoansalehnasution98@gmail.com>
2018-06-01 23:54:51 +0700

    add join & cancel actions to events

--- components/Event.js
+++ components/Event.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, TouchableOpacity, Alert } from 'react-native';
 import { Container, Content, Text, List, ListItem, Left, Thumbnail, Icon, Body, H1, H3 } from 'native-base';
 import { Col, Grid } from "react-native-easy-grid";
 import { auth, db } from '../firebase/config';
@@ -11,13 +11,33 @@ class EventScreen extends Component {
         super(props);
 
         this.state = {
+            isUserLogin: false,
+            userKey: null,
+            isUserMember: false,
             event: null,
             eventKey: this.props.navigation.state.params.event_key
         }
+
+        this.fetchEventData = this.fetchEventData.bind(this);
+        this.checkIsUserMember = this.checkIsUserMember.bind(this);
+        this.handleCancelJoinEvent = this.handleCancelJoinEvent.bind(this);
+        this.handleRequestJoinEvent = this.handleRequestJoinEvent.bind(this);
+        this.showAlertMessage = this.showAlertMessage.bind(this);
     }
 
     componentDidMount () {
+        auth.onAuthStateChanged(user => {
+            if (user) {
+                this.fetchEventData(true, user.uid);
+            } else {
+                this.fetchEventData(false);
+            }
+        });
+    }
+
+    fetchEventData (loginStatus, userKey) {
         let eventRef = db.ref('/events/'+this.state.eventKey);
+
         eventRef.on('value', (data) => {
             let groupName = '';
             let groupLocation = '';
@@ -51,15 +71,103 @@ class EventScreen extends Component {
                             host: groupAdmin,
                             total_members: totalMembers
                         };
-                        this.setState({ event: result });
+                        if (loginStatus) { 
+                            this.checkIsUserMember(loginStatus, userKey, result)
+                        } else {
+                            this.setState({ isUserLogin: loginStatus, event: result });
+                        }
                     }
                 });
             });
         })
     }
 
+    checkIsUserMember (loginStatus, userKey, eventData) {
+        let membersRef = db.ref('/events/'+this.state.eventKey+'/members');
+        let membersKey = [];
+
+        membersRef.on('value', (data) => {
+
+            if (data.val()) {
+                Object.keys(data.val()).map((m,i) => membersKey.push(m));
+            }
+
+            if (membersKey.length > 0) {
+                for (let i=0; i<membersKey.length; i++) {
+                    if (membersKey[i] === userKey) {
+                        this.setState({
+                            isUserLogin: loginStatus,
+                            userKey: userKey,
+                            isUserMember: true,
+                            event: eventData
+                        });
+                    }
+                    if (i === (membersKey.length-1) && !this.state.isUserMember) {
+                        this.setState({
+                            isUserLogin: loginStatus,
+                            userKey: userKey,
+                            event: eventData
+                        });
+                    }
+                }
+            } else {
+                this.setState({
+                    isUserLogin: loginStatus,
+                    userKey: userKey,
+                    event: eventData
+                });
+            }
+        });
+    }
+
+    handleCancelJoinEvent (isUserMember, userKey) {
+        if (!isUserMember) {
+            this.showAlertMessage('Peringatan', 'Kamu BELUM ikut event ini !');
+        } else {
+            let membersRef = db.ref('/events/'+this.state.eventKey+'/members/'+userKey);
+
+            membersRef.on('value', (data) => {
+                let eventKeys = [];
+                if (data.val()) {
+                    Object.keys(data.val()).map((e,i) => eventKeys.push(e));
+                }
+                if (eventKeys.length > 1) {
+                    db.ref('/events/'+this.state.eventKey+'/members/'+userKey).remove();
+                    this.setState({ isUserMember: false });
+                } else {
+                    let eventRef = db.ref('/events/'+this.state.eventKey);
+                    eventRef.update({ members: false });
+                    this.setState({ isUserMember: false });
+                }
+            });
+        }
+    }
+
+    handleRequestJoinEvent (isUserMember, userKey) {
+        if (isUserMember) {
+            this.showAlertMessage('Peringatan', 'Kamu SUDAH ikut event ini !');
+        } else {
+            let eventRef = db.ref('/events/'+this.state.eventKey+'/members/'+userKey);
+            eventRef.set({ status: true });
+            this.setState({ isUserMember: true });
+        }
+    }
+
+    showAlertMessage (title, content) {
+        Alert.alert(
+            title,
+            content,
+            [
+              {text: 'Tutup', onPress: () => {
+                console.log('Tutup');
+              }}
+            ],
+            { cancelable: true }
+        );
+    }
+
     render () {
-        let { event } = this.state;
+        let { isUserLogin, userKey, isUserMember, event } = this.state;
         return (
             <Container>
                 { event && <Content>
@@ -75,21 +183,24 @@ class EventScreen extends Component {
                     <View style={styles.eventBox}>
                         <H1>{ (event.name).toUpperCase() }</H1>
                     </View>
-                    <Grid style={styles.joinBox}>
-                        <Col style={{ width: '70%' }}>
-                            <Text>Apakah kamu ingin ikut ?</Text>
-                        </Col>
-                        <Col>
-                            <TouchableOpacity>
-                                <Icon name='ios-close-circle-outline' />
-                            </TouchableOpacity>
-                        </Col>
-                        <Col>
-                            <TouchableOpacity>
-                                <Icon name='ios-checkmark-circle-outline' />
-                            </TouchableOpacity>
-                        </Col>
-                    </Grid>
+                    { (event.quota > event.total_members) &&
+                        <Grid style={styles.joinBox}>
+                            <Col style={{ width: '70%' }}>
+                                <Text>Apakah kamu ingin ikut ?</Text>
+                            </Col>
+                            <Col>
+                                <TouchableOpacity onPress={() => this.handleCancelJoinEvent(isUserMember, userKey)}>
+                                    { isUserMember && <Icon name='ios-close-circle-outline' /> }
+                                    { !isUserMember && <Icon name='md-close-circle' /> }
+                                </TouchableOpacity>
+                            </Col>
+                            <Col>
+                                <TouchableOpacity onPress={() => this.handleRequestJoinEvent(isUserMember, userKey)}>
+                                    { isUserMember && <Icon name='md-checkmark-circle' /> }
+                                    { !isUserMember && <Icon name='ios-checkmark-circle-outline' /> }
+                                </TouchableOpacity>
+                            </Col>
+                        </Grid> }
                     <List>
                         <ListItem avatar>
                             <Left>
